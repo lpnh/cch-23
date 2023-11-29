fe5a5a0aec606cafcffc780fb70d3d1f1cd0438d
lpnh <lipe.paniguel@gmail.com>
2023-11-29 11:00:24 -0300

    add solution for day -1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 use axum::{routing::get, Router};
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use axum::response::IntoResponse;
+use axum::http::StatusCode;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let router = Router::new()
+        .route("/", get(home))
+        .route("/-1/error", get(fake_error));
 
     Ok(router.into())
+}
+
+async fn home() -> &'static str {
+    "ho ho ho!"
+}
+
+async fn fake_error() -> impl IntoResponse {
+    let status = StatusCode::INTERNAL_SERVER_ERROR;
+    let body = "ho ho... oh no!";
+
+    (status, body)
 }
